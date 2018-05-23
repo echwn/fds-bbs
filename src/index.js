956ee21445d1dfb5f00a1f1c1c0bb4cea4c2a08f
Lee Chaewon <lchaewn@gmail.com>
2018-05-23 12:29:35 +0900

    async await

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import axios from 'axios';
 
-axios.get('http://localhost:3000/posts').then(res => {
-  const listFragment = document.importNode(document.querySelector('#post-list').content, true);
-  res.data.forEach(post => {
-    const fragment = document.importNode(document.querySelector('#post-item').content, true);
-    const pEl = fragment.querySelector('.post-item__title');
-    pEl.textContent = post.title;
-    listFragment.querySelector('.post-list').appendChild(fragment);
-  })
+const rootEl = document.querySelector('.root');
+const templates = {
+  postLIst: document.querySelector('#post-list').content,
+  postItem: document.querySelector('#post-item').content
+}
 
-  document.querySelector('.root').appendChild(listFragment);
-})
+async function indexPage() {
+  const res = await axios.get('http://localhost:3000/posts');
+  const listFragment = document.importNode(templates.postList, true);
+    res.data.forEach(post => {
+      const fragment = document.importNode(templates.postItem, true);
+      const pEl = fragment.querySelector('.post-item__title');
+      pEl.textContent = post.title;
+      listFragment.querySelector('.post-list').appendChild(fragment);
+    })
+
+    rootEl.appendChild(listFragment);
+}
