6d5ce681460443372ed783ed917d8a3a2891c0e9
Lee Chaewon <lchaewn@gmail.com>
2018-05-23 13:06:16 +0900

    게시글 페이지 추가

--- src/index.js
+++ src/index.js
@@ -3,18 +3,41 @@ import axios from 'axios';
 const rootEl = document.querySelector('.root');
 const templates = {
   postLIst: document.querySelector('#post-list').content,
-  postItem: document.querySelector('#post-item').content
+  postItem: document.querySelector('#post-item').content,
+  postContent: document.querySelector('#post-content').content,
+}
+
+function render(fragment) {
+  rootEl.textContent = '';
+  rootEl.appendChild(listFragment);
 }
 
 async function indexPage() {
   const res = await axios.get('http://localhost:3000/posts');
   const listFragment = document.importNode(templates.postList, true);
-    res.data.forEach(post => {
-      const fragment = document.importNode(templates.postItem, true);
-      const pEl = fragment.querySelector('.post-item__title');
-      pEl.textContent = post.title;
-      listFragment.querySelector('.post-list').appendChild(fragment);
-    })
+  res.data.forEach(post => {
+    const fragment = document.importNode(templates.postItem, true);
+    const pEl = fragment.querySelector('.post-item__title');
+    pEl.textContent = post.title;
+    pEl.addEventListener('click', e => {
+      postContentPage(post.id);
+    });
+    listFragment.querySelector('.post-list').appendChild(fragment);
+  })
 
-    rootEl.appendChild(listFragment);
+  render(listFragment);
 }
+
+async function postContentPage(postId) {
+  const res = await axios.get(`http//localhost:3000/posts/${postId}`);
+  const fragment = document.importNode(templates.postContent, true);
+  fragment.querySelector('.post-item__title').textContent = res.data.title;
+  fragment.querySelector('.post-content__body').textContent = res.data.body;
+  fragment.querySelector('.post-content__back-btn').addEventListener('click', e => {
+    indexPage();
+  });
+
+  render(fragment);
+}
+
+indexPage();
