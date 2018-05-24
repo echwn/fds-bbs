87eeae050a0b170e9bfba993b9b2323b62ba007b
Lee Chaewon <lchaewn@gmail.com>
2018-05-24 11:07:19 +0900

    댓글 보기 기능

--- src/index.js
+++ src/index.js
@@ -23,6 +23,8 @@ const templates = {
   postContent: document.querySelector('#post-content').content,
   login: document.querySelector('#login').content,
   postForm: document.querySelector('#post-form').content,
+  comments: document.querySelector('#comments').content,
+  commentItem: document.querySelector('#comment-item').content,
 }
 
 function render(fragment) {
@@ -69,6 +71,17 @@ async function postContentPage(postId) {
     indexPage();
   })
 
+  if (localStorage.getItem('token')) {
+    const commentsFragment = document.importNode(templates.comments, true);
+    const commentsRes = await postAPI.get(`/posts/${postId}/comments`);
+    commentsRes.data.forEach(comment => {
+      const itemFragment = document.importNode(templates.commentItem, true);
+      itemFragment.querySelector('.comment-item__body').textContent = comment.body;
+      commentsFragment.querySelector('.comments__list').appendChild(itemFragment);
+    })
+    fragment.appendChild(commentsFragment);
+  }
+
   render(fragment);
 }
 
@@ -86,6 +99,7 @@ async function loginPage() {
     login(res.data.token);
     indexPage();
   })
+
   render(fragment);
 }
 
